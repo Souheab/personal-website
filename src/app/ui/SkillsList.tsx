6d645d1cbe5171a482e51b77b57fb3f96008f016
Souheab <souheab@protonmail.com>
2024-05-21 19:51:00 -0400

    Add react-page-scroller and refactor code

--- src/app/ui/SkillsList.tsx
+++ src/app/ui/SkillsList.tsx
@@ -6,7 +6,7 @@ interface SkillsListProps {
 export default function SkillsList(props: SkillsListProps) {
   return (
     <div className="border border-slate-400 m-2 my-5 rounded flex-1 p-5">
-      <div className="text-center text-xl text-slate-700">{props.skillsListTitle}</div>
+      <div className="text-center text-xl text-slate-700 font-bold">{props.skillsListTitle}</div>
       <div className="flex justify-center">
         <div className="p-4 inline-flex flex-col">
             {props.skillsListItems}
