58403b7da4917efff7f5822d17bcba46eb7bce7f
Souheab <souheab@protonmail.com>
2024-07-04 20:20:38 -0400

    Get particles on intro slides working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,19 @@
 "use client";
-import { useEffect, useState } from "react";
 import IntroPage from "./ui/IntroPage";
 import SkillsPage from "./ui/SkillsPage";
 import ContactPage from "./ui/ContactPage";
-import ReactPageScroller from "react-page-scroller";
 import ProjectsPage from "./ui/ProjectsPage";
+import ParticlesContainer from "./ui/ParticlesContainer";
 
 export default function Home() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 640);
-    };
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      {isMobile ? (
-        <>
-          <IntroPage />
-          <SkillsPage />
-          <ProjectsPage />
-          <ContactPage />
-        </>
-      ) : (
-        <ReactPageScroller>
-          <IntroPage />
-          <SkillsPage />
-          <ProjectsPage />
-          <ContactPage />
-        </ReactPageScroller>
-      )}
-    </main>
+    <>
+      <ParticlesContainer />
+      <IntroPage />
+      <SkillsPage />
+      <ProjectsPage />
+      <ContactPage />
+    </>
   );
 }
--- src/app/ui/IntroPage.tsx
+++ src/app/ui/IntroPage.tsx
@@ -1,9 +1,9 @@
 export default function IntroPage() {
   return (
-    <section className="panel intro-section-div min-h-screen flex items-center justify-center sm:p-24 p-6 text-slate-200 bg-violet-950">
-      <div className="flex flex-col sm:flex-row gap-8 items-center justify-center animate-fade-in">
+    <section className="panel intro-section-div min-h-screen relative flex items-center justify-center sm:p-24 p-6 text-slate-200">
+      <div className="flex flex-col sm:flex-row gap-8 items-center justify-center">
         <div className="inline-flex flex-col gap-4">
-          <div className="name-text animation-fade text-3xl sm:text-6xl">
+          <div className="name-text text-3xl sm:text-6xl">
             Hi, I am Souheab
           </div>
           <div className="text-xl sm:text-2xl text-slate-300">
